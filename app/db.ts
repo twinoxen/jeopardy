556425c5ee2eebd8a92a899c1d4edb7baf6c7d2d
twinoxen <1854037+twinoxen@users.noreply.github.com>
2021-09-25 11:54:47 -0400

    connect firestore config

--- app/db.ts
+++ app/db.ts
@@ -1,4 +1,5 @@
 import fb from './fb';
+import firebaseConfig from '../firebase.json'
 import {
   getFirestore,
   connectFirestoreEmulator,
@@ -10,25 +11,23 @@ const db = getFirestore(fb);
 
 if (typeof window !== 'undefined') {
   if (window.location.hostname === 'localhost') {
-    connectFirestoreEmulator(db, 'localhost', 8080);
+    connectFirestoreEmulator(db, 'localhost', firebaseConfig.emulators.firestore.port);
   }
 }
 
-async function test() {
-  try {
-    const docRef = await addDoc(collection(db, 'users'), {
-      first: 'Alan',
-      middle: 'Mathison',
-      last: 'Turing',
-      born: 1912,
-    });
+// async function test() {
+//   try {
+//     const docRef = await addDoc(collection(db, 'users'), {
+//       first: 'Alan',
+//       middle: 'Mathison',
+//       last: 'Turing',
+//       born: 1912,
+//     });
 
-    console.log('Document written with ID: ', docRef.id);
-  } catch (e) {
-    console.error('Error adding document: ', e);
-  }
-}
-
-test()
+//     console.log('Document written with ID: ', docRef.id);
+//   } catch (e) {
+//     console.error('Error adding document: ', e);
+//   }
+// }
 
 export default db;
